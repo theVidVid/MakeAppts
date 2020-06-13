21c2a78d21ccdcd3758031f0d635423924792da1
Ian Vidaurre <ianjvidaurre@gmail.com>
2020-06-12 23:11:44 -0700

    Created basic project structure.

---
+++ app/main.py
@@ -0,0 +1,23 @@
+"""
+Welcome to Appointment Maker.
+-------------------------------------------------------------------------------
+Automate the inputing of appointment data all into one easy to use script.
+===============================================================================
+Information needed:
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+1. Name of Union Member
+2. Name of Union 
+3. Date of Birth
+4. Telephone, H=Home, C=Cellphone
+5. Email Address, Must contain valid name@emailservice.com format
+6. Address
+7. Name of Primary Beneficiary
+8. Name of Contingent Beneficiary
+9. A section for any additional notes.
+10. Date of Appointment
+11. Time of Appointment
+12. Name of file.
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+After info is submitted, program will store info as a .txt file in a directory
+labeled APPOINTMENTS. 
+"""
