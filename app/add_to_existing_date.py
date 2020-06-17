520e1358d0811e6336545ac45b3a379338ee16e7
Ian Vidaurre <ianjvidaurre@gmail.com>
2020-06-16 18:32:51 -0700

    Created a add_to_existing_date.py script that will append user input for the given file that has already been created and edited some of the questions.

--- app/add_to_existing_date.py
+++ app/add_to_existing_date.py
@@ -1,28 +1,8 @@
-"""
-Welcome to Appointment Maker.
--------------------------------------------------------------------------------
-Automate the inputing of appointment data all into one easy to use script.
-===============================================================================
-Information needed:
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-1. Name of Union Member
-2. Name of Union 
-3. Date of Birth
-4. Telephone, H=Home, C=Cellphone
-5. Email Address, Must contain valid name@emailservice.com format
-6. Address
-7. Name of Primary Beneficiary
-8. Name of Contingent Beneficiary
-9. A section for any additional notes.
-10. Date of Appointment
-11. Time of Appointment
-12. Name of file.
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-After info is submitted, program will store info as a .txt file in a directory
-labeled APPOINTMENTS. 
-"""
+print("Welcome to Appointment Maker!")
+print("======================================================================")
 date = input(
-    "What is the date of the appointment? eg(weekday-day.txt): ")
+    "What is the date of the appointment you're adding to?" 
+    "eg(weekday-day.txt): ")
 time = input("What time is the appointment: eg(12PM) ")
 union_member = input("What is the name of the union member? ")
 union_name = input("What is the name of the union? ")
@@ -36,17 +16,16 @@ contingent = input("Who is the contingent beneficiary? ")
 child_safe_kit = input("How many Child Safe Kits were ordered? ")
 # folder = "C:\Users\ianbt\Desktop\Appts"
 
-with open(date, "w") as f_obj:
-    f_obj.write("----------------------------------------------------------\n")
+filename = date
+with open(filename, "a") as f_obj:
     f_obj.write("----------------------------------------------------------\n")
     f_obj.write(f"Time: {time}\n")
     f_obj.write(f"Name: {union_member}\n")
-    f_obj.write(f"Union Name: {union_name}\n")
     f_obj.write(f"Date of Birth: {birthdate}\n")
     f_obj.write(f"Telephone: {telephone}\n")
     f_obj.write(f"Email: {email}\n")
     f_obj.write(f"Address: {home_address}\n")
-    f_obj.write(f"Primary Beneficiary: {primary_beneficiary}\n")
+    f_obj.write(f"Beneficiary: {primary_beneficiary}\n")
     f_obj.write(f"Contingent Beneficiary: {contingent}\n")
     f_obj.write(f"Child Safe Kits: {child_safe_kit}\n")
     f_obj.write("----------------------------------------------------------\n")
