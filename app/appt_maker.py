62520da15f933a4c56273f03aebb94b76d1ea3e6
Ian Vidaurre <Ianjvidaurre@gmail.com>
2020-06-18 18:02:36 -0700

    Updated appt_maker.py script to include both win10 and linux filepaths and used the access() method from the os module to check for existence of either filepath. Which ever paths exits will set it to filepath to open filename in write mode through context manager.

--- app/appt_maker.py
+++ app/appt_maker.py
@@ -1,7 +1,7 @@
 """
 Welcome to Appointment Maker.
 -------------------------------------------------------------------------------
-Automate the inputing of appointment data all into one easy to use script.
+Automate the inputting of appointment data all into one easy to use script.
 ===============================================================================
 Information needed:
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -22,27 +22,41 @@ After info is submitted, program will store info as a .txt file in a directory
 labeled APPOINTMENTS. 
 """
 from pathlib import Path
-
+import os
+print("Welcome to Appointment Maker!")
+print("===============================================================================================================")
 date = input("What is the date of the appointment? eg(weekday-day.txt): ")
 time = input("What time is the appointment? eg(12PM): ")
 union_member = input("What is the name of the union member? ")
 union_name = input("What is the name of the union? ")
 birthdate = input("What is the union member's date of birth? mm-dd-yy ")
-telephone = input(
-    "What is the union member's primary telephone? "
-    "eg(111-222-3333): "
-)
+telephone = input("What is the union member's primary telephone? " "eg(111-222-3333): ")
 email = input("What is the union member's email address? ")
 home_address = input("What is the union member's mailing address? ")
 primary_beneficiary = input("Who is the union member's primary beneficiary? ")
 contingent = input("Who is the contingent beneficiary? ")
 child_safe_kit = input("How many Child Safe Kits were ordered? ")
 
-filepath = Path("/mnt/c/Users/ianbt/desktop/Appts")
+# Initialize filepath to an empty string.
+filepath = ""
+
+# Wrote paths for each OS.
+windows_path = Path("/mnt/c/Users/ianbt/desktop/Appts")
+linux_path = Path("/home/thevidvid/Desktop/Appts")
+
+# Used the os.access() method to check the existence of either windows or linux path and if True, sets it to that path.
+if os.access(windows_path, os.F_OK):
+    filepath = Path("/mnt/c/Users/ianbt/desktop/Appts")
+elif os.access(linux_path, os.F_OK):
+    filepath = Path("/home/thevidvid/Desktop/Appts")
+else:
+    print(
+        "Error: No matching directory found. Please create an Appts directory in your desktop."
+    )
 filename = f"{date}"
-with open(filepath/filename, 'w') as f_obj:
-    f_obj.write("----------------------------------------------------------\n")
-    f_obj.write("----------------------------------------------------------\n")
+with open(filepath / filename, "w") as f_obj:
+    f_obj.write("---------------------------------------------------------------------------------------------------\n")
+    f_obj.write("---------------------------------------------------------------------------------------------------\n")
     f_obj.write(f"Time: {time}\n")
     f_obj.write(f"Name: {union_member}\n")
     f_obj.write(f"Union Name: {union_name}\n")
@@ -53,4 +67,4 @@ with open(filepath/filename, 'w') as f_obj:
     f_obj.write(f"Primary Beneficiary: {primary_beneficiary}\n")
     f_obj.write(f"Contingent Beneficiary: {contingent}\n")
     f_obj.write(f"Child Safe Kits: {child_safe_kit}\n")
-    f_obj.write("----------------------------------------------------------\n")
+    f_obj.write("---------------------------------------------------------------------------------------------------\n")
