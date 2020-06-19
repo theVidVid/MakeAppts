3fdfeaf18b9fb5029747b4e5db5fd79c344a77e1
Ian Vidaurre <Ianjvidaurre@gmail.com>
2020-06-18 18:27:00 -0700

    Updated appt_updater.py script to check existence of a win10 or Linux path and implement the filepath based on which returns true.

--- app/appt_updater.py
+++ app/appt_updater.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import os
 
 print("Welcome to Appointment Maker!")
-print("======================================================================")
+print("===============================================================================================================")
 date = input(
     "What is the date of the appointment you're adding to? "
     "eg(weekday-day.txt): " 
@@ -20,10 +21,25 @@ primary_beneficiary = input("Who is the union member's primary beneficiary? ")
 contingent = input("Who is the contingent beneficiary? ")
 child_safe_kit = input("How many Child Safe Kits were ordered? ")
 
-filepath = Path("/mnt/c/Users/ianbt/desktop/Appts")
+# Initialize filepath to an empty string.
+filepath = ""
+
+# Wrote paths for each OS.
+windows_path = Path("/mnt/c/Users/ianbt/desktop/Appts")
+linux_path = Path("/home/thevidvid/Desktop/Appts")
+
+# Used the os.access() method to check the existence of either windows or linux path and if True, sets it to that path.
+if os.access(windows_path, os.F_OK):
+    filepath = Path("/mnt/c/Users/ianbt/desktop/Appts")
+elif os.access(linux_path, os.F_OK):
+    filepath = Path("/home/thevidvid/Desktop/Appts")
+else:
+    print(
+        "Error: No matching directory found. Please create an Appts directory in your desktop."
+    )
 filename = f"{date}"
 with open(filepath/filename, "a") as f_obj:
-    f_obj.write("----------------------------------------------------------\n")
+    f_obj.write("---------------------------------------------------------------------------------------------------\n")
     f_obj.write(f"Time: {time}\n")
     f_obj.write(f"Name: {union_member}\n")
     f_obj.write(f"Date of Birth: {birthdate}\n")
@@ -33,4 +49,4 @@ with open(filepath/filename, "a") as f_obj:
     f_obj.write(f"Beneficiary: {primary_beneficiary}\n")
     f_obj.write(f"Contingent Beneficiary: {contingent}\n")
     f_obj.write(f"Child Safe Kits: {child_safe_kit}\n")
-    f_obj.write("----------------------------------------------------------\n")
+    f_obj.write("---------------------------------------------------------------------------------------------------\n")
